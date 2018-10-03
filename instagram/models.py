7aa34163f711f6e0673abd4aabda143a9c053f58
Michel Atieno <mishqamish@gmail.com>
2018-10-03 17:37:17 +0300

    Added models profile and image

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to ='prof_pictures/')
+    bio = models.CharField(max_length = 100)
+
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='prof_pictures/')
+    image_name = models.CharField(max_length = 50)
+    image_caption =  models.CharField(max_length = 50)
+    profile = models.ForeignKey(Profile)
+    likes = models.BooleanField(default=False)
+    comments = models.CharField(max_length = 100)
+
