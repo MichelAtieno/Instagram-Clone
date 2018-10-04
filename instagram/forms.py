bdb9cb4d1a1e6734b460645c54b9335ac80e15cc
Michel Atieno <mishqamish@gmail.com>
2018-10-04 15:31:21 +0300

    Registration form is root page and redirects to sign in page after registration

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField()
 
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text = 'Required')
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    # def save(self, commit=True):
+    #     user = super(RegistrationForm, self).save(commit=False)
+    #     user.email = cleaned_data['email']
+        
+    #     if commit:
+    #         user.save()
+
+    #     return user
--- instagram/views.py
+++ instagram/views.py
@@ -1,10 +1,26 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile, Image
+from .forms import RegistrationForm 
 
 # Create your views here.
 def register(request):
-    return render(request, 'registration/registration_form.html')
+        if request.method == 'POST':
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                return HttpResponse('Confirm email to complete registration')
+        else:
+            form = RegistrationForm()
+            return render(request, 'registration/registration_form.html', {'form':form})
+
+
+    
 
 
 
