f6334c01f2f3161054cb5a779b9384f005b72cd8
Michel Atieno <mishqamish@gmail.com>
2018-10-08 15:31:21 +0300

    Able to get confirmation email

--- instagram/views.py
+++ instagram/views.py
@@ -25,11 +25,20 @@ def register(request):
                 user = form.save(commit=False)
                 user.is_active = False
                 user.save()
-                current_site =get_current_site(request)
-                to_email = form.cleaned_data['email']
-                send_welcome_email(user, current_site, to_email)
-                #return redirect('accounts/login')
-                return HttpResponse('Confirm email address')
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your blog account.'
+                message = render_to_string('registration/activate.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token':account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(
+                            mail_subject, message, to=[to_email]
+                )
+                email.send()
+                return HttpResponse('Please confirm your email address to complete the registration')
         else:
             form = RegistrationForm()
             return render(request, 'registration/registration_form.html', {'form':form})
